dcdcd464a1752895760533a1ccdf0da08a2e21f9
Chr_ <chr@chrxw.com>
2021-09-08 22:11:04 +0800

    添加功能开关，优化实现

--- Steam/Show_English_Name.js
+++ Steam/Show_English_Name.js
@@ -2,9 +2,9 @@
 // @name         Show_English_Name
 // @name:zh-CN   Steam显示英文游戏名
 // @namespace    https://blog.chrxw.com
-// @version      1.2
-// @description  在商店页显示双语游戏名称，点击名称可以复制。
-// @description:zh-CN  在商店页显示双语游戏名称，点击名称可以复制。
+// @version      1.7
+// @description  在商店页显示双语游戏名称，双击名称可以快捷搜索。
+// @description:zh-CN  在商店页显示双语游戏名称，双击名称可以快捷搜索。
 // @author       Chr_
 // @include      /https://store\.steampowered\.com\/app\/\d+/
 // @require      https://greasyfork.org/scripts/431423-async-requests/code/Async_Requests.js
@@ -13,38 +13,54 @@
 // @icon         https://blog.chrxw.com/favicon.ico
 // @grant        GM_setClipboard
 // @grant        GM_xmlhttpRequest
+// @grant        GM_registerMenuCommand
 // ==/UserScript==
 
 
 (() => {
     'use strict';
-
+    let mode = window.localStorage['sen_mode'] ?? 'c(e)';
+    let pure = window.localStorage['sen_pure'] ?? '关';
     let appid = (window.location.pathname.match(/\/app\/(\d+)/) ?? [null, null])[1];
-
     if (appid === null) { return; }
-
     $http.get(`https://store.steampowered.com/api/appdetails?appids=${appid}&l=english`)
         .then(json => {
-
             let data = json[appid];
-
             if (data.success !== true) { return; }
-
-            let en_title = data.data.name;
-
-            en_title = en_title.replace(/ on Steam$/, '');
-
+            let name_en = data.data.name;
             let t = setInterval(() => {
-                let ele = document.getElementById('appHubAppName');
-                if (ele != null) {
+                let ele_title = document.getElementById('appHubAppName');
+                if (ele_title != null) {
                     clearInterval(t);
-                    let raw_title = ele.textContent
-                    if (raw_title.toLowerCase() != en_title.toLowerCase()) {
-                        ele.textContent = `${raw_title} (${en_title})`;
-                        ele.title = '点击复制';
+                    let ele_path = document.querySelector('div.blockbg>a:last-child');
+                    let name_cur = ele_title.textContent
+                    if (name_cur.toLowerCase() != name_en.toLowerCase()) {
+                        if (pure === '开') {
+                            name_en = pureName(name_en);
+                            name_cur = pureName(name_cur);
+                        }
+                        let name_new = '';
+                        if (mode === 'e(c)') {
+                            name_new = `${name_en} (${name_cur})`;
+                        } else {
+                            name_new = `${name_cur} (${name_en})`;
+                        }
+                        ele_title.textContent = name_new;
+                        if (ele_path !== null) {
+                            ele_path.textContent = name_new;
+                        }
                     }
-                    ele.addEventListener('click', () => {
-                        GM_setClipboard(ele.textContent, { type: 'text', mimetype: 'text/plain' });
+                    ele_title.title = '双击快捷搜索';
+                    ele_title.addEventListener('dblclick', () => {
+                        ShowConfirmDialog(`要使用的搜索关键词？`, '', name_cur, name_en)
+                            .done(() => {
+                                window.open(`https://store.steampowered.com/search/?term=${name_cur}`);
+                            })
+                            .fail((stats) => {
+                                if (stats) {
+                                    window.open(`https://store.steampowered.com/search/?term=${name_en}`);
+                                }
+                            })
                     });
                 }
             }, 500);
@@ -52,5 +68,15 @@
         .catch(err => {
             console.error(err);
         });
+    GM_registerMenuCommand(`切换显示格式：【${mode === 'c(e)' ? '原名 (英文名)' : '英文名 (原名)'}】`, () => {
+        window.localStorage['sen_mode'] = mode === 'c(e)' ? 'e(c)' : 'c(e)';
+        window.location.reload();
+    })
+    GM_registerMenuCommand(`过滤特殊符号：【${pure}】`, () => {
+        window.localStorage['sen_pure'] = pure === '开' ? '关' : '开';
+        window.location.reload();
+    })
+    function pureName(str) {
+        return str.replace(/[《》™©®]/g, '');
+    }
 })();
-
