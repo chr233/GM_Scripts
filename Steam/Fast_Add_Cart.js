6e0d2c0c93c7e85b8cd91ea6ba17ebfca6f30d98
Chr_ <chr@chrxw.com>
2021-09-13 00:36:21 +0800

    支持批量导入导出购物车

--- Steam/Fast_Add_Cart.js
+++ Steam/Fast_Add_Cart.js
@@ -2,21 +2,26 @@
 // @name         Fast_Add_Cart
 // @name:zh-CN   Steam快速添加购物车
 // @namespace    https://blog.chrxw.com
-// @version      1.8
+// @version      2.1
 // @description  在商店页显示双语游戏名称，双击名称可以快捷搜索。
 // @description:zh-CN  在商店页显示双语游戏名称，双击名称可以快捷搜索。
 // @author       Chr_
 // @include      /https://store\.steampowered\.com\/search/.*/
 // @include      /https://store\.steampowered\.com\/publisher/.*/
+// @include      /https://store\.steampowered\.com\/cart/.*/
 // @license      AGPL-3.0
 // @icon         https://blog.chrxw.com/favicon.ico
 // @grant        GM_addStyle
+// @grant        GM_setClipboard
 // ==/UserScript==
 
 (async () => {
     'use strict';
     //初始化
-    if (window.location.pathname.indexOf('search') !== -1) { //搜索页
+
+    let pathname = window.location.pathname;
+
+    if (pathname.indexOf('search') !== -1) { //搜索页
         let t = setInterval(() => {
             let container = document.getElementById('search_resultsRows');
             if (container != null) {
@@ -31,7 +36,7 @@
                 });
             }
         }, 500);
-    } else { //发行商主页
+    } else if (pathname.indexOf('publisher') !== -1) { //发行商主页
         let t = setInterval(() => {
             let container = document.getElementById('RecommendationsRows');
             if (container != null) {
@@ -49,6 +54,128 @@
                 });
             }
         }, 500);
+    } else { //购物车页
+        let continer = document.querySelector('div.cart_area_body');
+
+        let genBr = () => { return document.createElement('br'); };
+        let genBtn = (text, onclick) => {
+            let btn = document.createElement('button');
+            let spn = document.createElement('span');
+            btn.textContent = text;
+            btn.className = 'btn_medium btnv6_blue_hoverfade fac_cartbtns';
+            btn.addEventListener('click', onclick);
+            return btn;
+        };
+        let inputBox = document.createElement('textarea');
+
+        inputBox.className = 'fac_inputbox';
+        inputBox.placeholder = ['一行一条, 支持的格式如下:',
+            '1. 商店链接: https://store.steampowered.com/app/xxx',
+            '2. DB链接:  https://steamdb.info/app/xxx',
+            '3. appID:   xxx a/xxx app/xxx',
+            '4. subID:       s/xxx sub/xxx',
+            '5. bundleID:    b/xxx bundle/xxx'
+        ].join('\n');
+
+
+        let btnArea = document.createElement('div');
+
+        let btnImport = genBtn('🔼批量导入购物车', async () => { inputBox.value = await importCart(inputBox.value); });
+        let btnExport = genBtn('🔽导出当前购物车', () => { inputBox.value = exportCart(); });
+        let btnCopy = genBtn('📋复制文本框内容', () => {
+            GM_setClipboard(inputBox.value, { type: 'text', mimetype: 'text/plain' });
+            showAlert('提示', '复制到剪贴板成功', true);
+        });
+        let btnHelp = genBtn('🔣帮助', () => {
+            showAlert('帮助', [
+                '<p>【🔼批量导入购物车】将当前购物车内容保存至文本框。</p>',
+                '<p>【🔽导出当前购物车】按照文本框内容批量导入购物车。</p>',
+                '<p>【📋复制文本框内容】复制文本框内容(废话)。</p>',
+                '<p>【🔣帮助】显示没什么卵用的帮助。</p>',
+                '<p>【<a href=https://keylol.com/t747892-1-1 target="_blank">发布帖</a>】 【<a href=https://blog.chrxw.com/scripts.html target="_blank">脚本反馈</a>】</p>'
+            ].join('<br>'), true)
+        });
+
+        btnArea.appendChild(btnImport);
+        btnArea.appendChild(btnExport);
+        btnArea.appendChild(btnCopy);
+        btnArea.appendChild(btnHelp);
+
+
+        continer.appendChild(btnArea);
+        btnArea.appendChild(genBr());
+        btnArea.appendChild(genBr());
+        continer.appendChild(inputBox);
+
+    }
+    //导入购物车
+    function importCart(text) {
+        return new Promise(async (resolve, reject) => {
+            let regFull = new RegExp(/(app|a|bundle|b|sub|s)\/(\d+)/);
+            let regShort = new RegExp(/()(\d+)/);
+            let lines = [];
+            let dialog = showAlert('操作中……', '正在导入购物车...', true);
+            for (let line of text.split('\n')) {
+                let match = line.match(regFull) ?? line.match(regShort);
+                if (!match) {
+                    let tmp = line.split('#')[0];
+                    lines.push(`${tmp} #格式有误`);
+                    continue;
+                }
+                let [_, type, subID] = match;
+                switch (type.toLowerCase()) {
+                    case '':
+                    case 'a':
+                    case 'app':
+                        type = 'app';
+                        break;
+                    case 's':
+                    case 'sub':
+                        type = 'sub';
+                        break;
+                    case 'b':
+                    case 'bundle':
+                        type = 'bundle';
+                        break;
+                    default:
+                        let tmp = line.split('#')[0];
+                        lines.push(`${tmp} #格式有误`);
+                        continue;
+                }
+
+                if (type === 'sub' || type === 'bundle') {
+                    let [succ, msg] = await addCart(type, subID, '');
+                    lines.push(`${type}/${subID} #${msg}`);
+
+                } else {
+                    try {
+                        let subInfos = await getGameSubs(subID);
+                        let [sID, subName] = subInfos[0];
+                        let [succ, msg] = await addCart('sub', sID, subID);
+                        lines.push(`${type}/${subID} #${subName} ${msg}`);
+                    } catch (e) {
+                        lines.push(`${type}/${subID} #未找到可用SUB`);
+                    }
+                }
+                let d = showAlert('操作中……', `<p>${lines.join('</p><p>')}</p>`, true);
+                setTimeout(() => { d.Dismiss(); }, 1200);
+            }
+            dialog.Dismiss();
+            resolve(lines.join('\n'));
+        });
+    }
+    //导出购物车
+    function exportCart() {
+        let data = [];
+        for (let item of document.querySelectorAll('div.cart_item_list>div.cart_row ')) {
+            let id = item.getAttribute('data-ds-appid') ?? item.getAttribute('data-ds-bundleid');
+            if (item.hasAttribute('data-ds-bundleid')) {
+                data.push(`bundle/${id}`);
+            } else if (item.hasAttribute('data-ds-appid')) {
+                data.push(`app/${id}`);
+            }
+        }
+        return data.join('\n');
     }
     //添加按钮
     function addButton(element) {
@@ -68,7 +195,8 @@
         btn.className = 'fac_listbtns';
         btn.textContent = '🛒';
         element.appendChild(btn);
-    }    //添加按钮
+    }
+    //添加按钮
     function addButton2(element) {
         if (element.getAttribute('added') !== null) { return; }
         element.setAttribute('added', '');
@@ -87,7 +215,7 @@
         btn.textContent = '🛒';
         element.appendChild(btn);
     }
-    //列表按钮点击
+    //选择SUB
     async function chooseSubs(appID) {
         let dialog = showAlert('操作中……', '<p>读取可用SUB</p>', true);
         getGameSubs(appID)
@@ -99,7 +227,7 @@
                     console.log(subInfos);
                     if (subInfos.length === 1) {
                         let [subID, subName] = subInfos[0];
-                        await addCart(subID, appID);
+                        await addCart('sub', subID, appID);
                         let done = showAlert('添加购物车成功', `<p>${subName}</p>`, true);
                         setTimeout(() => { done.Dismiss(); }, 1200);
                         dialog.Dismiss();
@@ -120,7 +248,7 @@
                             btn.addEventListener('click', async () => {
                                 let dialog = showAlert('操作中……', `<p>添加 ${subName} 到购物车</p>`, true);
                                 dialog2.Dismiss();
-                                let [succ, msg] = await addCart(subID, appID);
+                                let [succ, msg] = await addCart('sub', subID, appID);
                                 let done = showAlert(msg, `<p>${subName}</p>`, succ);
                                 setTimeout(() => { done.Dismiss(); }, 1200);
                                 dialog.Dismiss();
@@ -176,7 +304,7 @@
         });
     }
     //添加购物车,只支持subID
-    function addCart(subID, appID) {
+    function addCart(type = 'sub', subID, appID = null) {
         window.localStorage['fac_subid'] = subID;
         return new Promise((resolve, reject) => {
             let data = {
@@ -184,8 +312,8 @@
                 originating_snr: "1_store-navigation__",
                 sessionid: document.cookie.replace(/(?:(?:^|.*;\s*)sessionid\s*\=\s*([^;]*).*$)|^.*$/, "$1"),
                 snr: "1_5_9__403",
-                subid: String(subID),
             }
+            data[`${type}id`] = String(subID);
             let s = [];
             for (let k in data) {
                 s += `${k}=${encodeURIComponent(data[k])}&`;
@@ -199,12 +327,16 @@
                 .then(async response => {
                     if (response.ok) {
                         let data = await response.text();
-                        let reg = new RegExp('app\/' + appID);
-                        if (data.search(reg) !== -1) {
+                        if (appID !== null) {
+                            let reg = new RegExp('app\/' + appID);
+                            if (data.search(reg) !== -1) {
+                                resolve([true, '添加购物车成功']);
+                            }
+                            else {
+                                resolve([false, '添加购物车失败']);
+                            }
+                        } else {
                             resolve([true, '添加购物车成功']);
-                        }
-                        else {
-                            resolve([false, '添加购物车失败']);
                         }
                     } else {
                         resolve([false, '网络请求失败']);
@@ -245,6 +377,12 @@ button.fac_listbtns {
   left: 300px;
   position: relative;
 }
+button.fac_cartbtns {
+  padding: 5px 10px;
+}
+button.fac_cartbtns:not(:last-child) {
+  margin-right: 15px;
+}
 a.search_result_row:hover button.fac_listbtns,
 div.recommendation:hover button.fac_publisherbtns {
   display: block;
@@ -253,5 +391,9 @@ button.fac_choose {
   padding: 1px;
   margin: 2px 5px;
 }
-
+textarea.fac_inputbox{
+  height: 120px;
+  resize: vertical;
+  font-size: 10px;
+}
 `);
