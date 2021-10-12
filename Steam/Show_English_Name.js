cf6ea0b8e3dd274989a2aba50a7ac4366d92e92a
Chr_ <chr@chrxw.com>
2021-10-12 21:05:07 +0800

    修复Violentmonkey下复制失败的问题

--- Steam/Show_English_Name.js
+++ Steam/Show_English_Name.js
@@ -4,7 +4,7 @@
 // @namespace       https://blog.chrxw.com
 // @supportURL      https://blog.chrxw.com/scripts.html
 // @contributionURL https://afdian.net/@chr233
-// @version         1.10
+// @version         1.11
 // @description     在商店页显示双语游戏名称，双击名称可以快捷搜索。
 // @description:zh-CN  在商店页显示双语游戏名称，双击名称可以快捷搜索。
 // @author          Chr_
@@ -18,9 +18,9 @@
 
 (() => {
     'use strict';
-    let mode = window.localStorage['sen_mode'] ?? 'c(e)';
-    let pure = window.localStorage['sen_pure'] ?? '关';
-    let appid = (window.location.pathname.match(/\/app\/(\d+)/) ?? [null, null])[1];
+    const mode = window.localStorage['sen_mode'] ?? 'c(e)';
+    const pure = window.localStorage['sen_pure'] ?? '关';
+    const appid = (window.location.pathname.match(/\/app\/(\d+)/) ?? [null, null])[1];
     if (appid === null) { return; }
     fetch(`https://store.steampowered.com/api/appdetails?appids=${appid}&l=english`)
         .then(async response => {
@@ -55,16 +55,17 @@
                         ele_title.addEventListener('dblclick', () => {
                             ShowConfirmDialog(`你想做什么呢？`, '', '复制游戏名', '搜索游戏名')
                                 .done(() => {
+                                    const setClipboard = (data) => { GM_setClipboard(data, 'text'); }
                                     if (name_cur == name_en) {
-                                        GM_setClipboard(name_cur, { type: 'text', mimetype: 'text/plain' });
+                                        setClipboard(name_cur);
                                     } else {
                                         ShowConfirmDialog(`要复制的游戏名称？`, '', name_cur, name_en)
                                             .done(() => {
-                                                GM_setClipboard(name_cur, { type: 'text', mimetype: 'text/plain' });
+                                                setClipboard(name_cur);
                                             })
                                             .fail((stats) => {
                                                 if (stats) {
-                                                    GM_setClipboard(name_en, { type: 'text', mimetype: 'text/plain' });
+                                                    setClipboard(name_en);
                                                 }
                                             });
                                     }
