23b04be637831b0debebf85abf14fa2219472529
Chr_ <chr@chrxw.com>
2021-11-11 12:23:57 +0800

    兼容暴力猴

--- GreenManGaming/GMG_Steam.js
+++ GreenManGaming/GMG_Steam.js
@@ -5,113 +5,118 @@
 // @supportURL      https://blog.chrxw.com/scripts.html
 // @contributionURL https://afdian.net/@chr233
 // @icon            https://blog.chrxw.com/favicon.ico
-// @version         1.1
+// @version         1.2
 // @description     快捷搜索steam商店
 // @description:zh-CN  快捷搜索steam商店
 // @author          Chr_
 // @license         AGPL-3.0
 // @connect         steampowered.com
 // @match           https://www.greenmangaming.com/*/games/*
+// @match           https://www.greenmangaming.com/games/*
 // @require         https://greasyfork.org/scripts/431430-search-steam-store/code/Search_Steam_Store.js
 // @grant           GM_xmlhttpRequest
 // @grant           GM_addStyle
 // ==/UserScript==
 
 (() => {
-    'use strict';
-    let GdivResult = null; //控件数组
+  'use strict';
+  let GdivResult = null; //控件数组
 
-    const ele = document.querySelector('h1.notranslate');
-
-    init(ele);
-
-    //显示搜索按钮
-    function init(ele) {
-        const keyword = ele.textContent.replace(/[-+=:;：；'"‘’“”]/g, ' ');
-        const btnSearch = document.createElement('button');
-        btnSearch.className = 'btnSearch';
-        btnSearch.textContent = '🔎';
-        btnSearch.addEventListener('mouseover', () => { btnSearch.textContent = '🔎 搜索Steam'; });
-        btnSearch.addEventListener('mouseout', () => { btnSearch.textContent = '🔎'; });
-        btnSearch.addEventListener('click', () => { showResult(keyword); });
-
-        ele.appendChild(btnSearch);
-        const divResult = document.createElement('div');
-        divResult.className = 'divResult';
-        divResult.style.display = 'none';
-        ele.appendChild(divResult);
-
-        GdivResult = divResult;
+  let t = setInterval(() => {
+    const title = document.querySelector('h1.notranslate');
+    const describe = document.querySelector('ul.navigate-options');
+    if (title !== null) {
+      clearInterval(t);
+      init(title, describe);
     }
+  }, 500);
 
-    //显示搜索结果
-    function showResult(keyword) {
-        searchStore(keyword, 'CN')
-            .then((result) => {
-                GdivResult.innerHTML = '';
-                GdivResult.style.display = '';
-                if (result.length === 0) {
-                    const btnRst = document.createElement('button');
-                    btnRst.textContent = '【快速搜索无结果,点击前往steam搜索页】';
-                    btnRst.addEventListener('click', () => { window.open(`https://store.steampowered.com/search/?term=${keyword}`); });
-                    GdivResult.appendChild(btnRst);
-                    return;
-                }
-                result.forEach(({ appID, isBundle, appName, appPrice, appUrl, appImg }) => {
-                    const btnRst = document.createElement('button');
-                    btnRst.title = `${isBundle ? "bundle" : "app"}/${appID}`;
-                    btnRst.addEventListener('click', () => { window.open(appUrl); });
+  //显示搜索按钮
+  function init(title, describe) {
+    const keyword = title.textContent.replace(/[-+=:;：；'"‘’“”]/g, ' ');
+    const btnSearch = document.createElement('button');
+    btnSearch.className = 'btnSearch';
+    btnSearch.textContent = '🔎';
+    btnSearch.addEventListener('mouseover', () => { btnSearch.textContent = '🔎 搜索Steam'; });
+    btnSearch.addEventListener('mouseout', () => { btnSearch.textContent = '🔎'; });
+    btnSearch.addEventListener('click', () => { showResult(keyword); });
 
-                    const btnName = document.createElement('p');
-                    btnName.textContent = `${appName}【${appPrice}】`
-                    btnRst.appendChild(btnName);
-                    btnRst.appendChild(document.createElement('br'));
+    (describe ?? title).appendChild(btnSearch);
+    const divResult = document.createElement('div');
+    divResult.className = 'divResult';
+    divResult.style.display = 'none';
+    title.appendChild(divResult);
 
-                    const btnImg = new Image();
-                    btnImg.src = appImg;
+    GdivResult = divResult;
+  }
 
-                    btnRst.appendChild(btnImg);
-                    GdivResult.appendChild(btnRst);
-                });
-            })
-            .catch((reason) => {
-                alert(reason);
-            });
-    }
+  //显示搜索结果
+  function showResult(keyword) {
+    searchStore(keyword, 'CN')
+      .then((result) => {
+        GdivResult.innerHTML = '';
+        GdivResult.style.display = '';
+        if (result.length === 0) {
+          const btnRst = document.createElement('button');
+          btnRst.textContent = '【快速搜索无结果,点击前往steam搜索页】';
+          btnRst.addEventListener('click', () => { window.open(`https://store.steampowered.com/search/?term=${keyword}`); });
+          GdivResult.appendChild(btnRst);
+          return;
+        }
+        result.forEach(({ appID, isBundle, appName, appPrice, appUrl, appImg }) => {
+          const btnRst = document.createElement('button');
+          btnRst.title = `${isBundle ? "bundle" : "app"}/${appID}`;
+          btnRst.addEventListener('click', () => { window.open(appUrl); });
+
+          const btnName = document.createElement('p');
+          btnName.textContent = `${appName}【${appPrice}】`
+          btnRst.appendChild(btnName);
+          btnRst.appendChild(document.createElement('br'));
+
+          const btnImg = new Image();
+          btnImg.src = appImg;
+
+          btnRst.appendChild(btnImg);
+          GdivResult.appendChild(btnRst);
+        });
+      })
+      .catch((reason) => {
+        alert(reason);
+      });
+  }
 })();
 
 
 //CSS表
 GM_addStyle(`
 .divResult {
-    top: 0;
-    position: relative;
-    width: 100%;
-    overflow-x: scroll;
-    overflow-y: hidden;
-    white-space: nowrap;
-  }
-  .divResult > button {
-    cursor: pointer;
-    color: #000;
-    font-size: 15px;
-  }
-  .divResult > button:not(:last-child) {
-    margin-right: 5px;
-  }
-  .divResult > button > p {
-    display: inline;
-    margin-left: 6px;
-  }
-  .divResult > button > img {
-    zoom: 1.5;
-    margin-top: 2px;
-  }
-  .btnSearch {
-    padding: 0 5px;
-    margin-left: 5px;
-    color: #000;
-    font-size: 15px;
-    padding: 2px;
-  }  
+  top: 0;
+  position: relative;
+  width: 100%;
+  overflow-x: scroll;
+  overflow-y: hidden;
+  white-space: nowrap;
+}
+.divResult > button {
+  cursor: pointer;
+  color: #000;
+  font-size: 15px;
+}
+.divResult > button:not(:last-child) {
+  margin-right: 5px;
+}
+.divResult > button > p {
+  display: inline;
+  margin-left: 6px;
+}
+.divResult > button > img {
+  zoom: 1.5;
+  margin-top: 2px;
+}
+.btnSearch {
+  padding: 1px;
+  margin-left: 5px;
+  color: #000;
+  font-size: 10px;
+}
 `);
