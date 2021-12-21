81dfe4698ea387fb84581a2095e8d4a19986ec2c
Chr_ <chr@chrxw.com>
2021-12-21 13:36:13 +0800

    修复bug

--- Bilibili/Remove_Live.js
+++ Bilibili/Remove_Live.js
@@ -2,45 +2,37 @@
 // @name         Remove_Live
 // @name:zh-CN   去除B站直播间播放器
 // @namespace    https://blog.chrxw.com
-// @version      1.0
+// @version      1.1
 // @description  去除B站多余的直播播放器（滑稽）
 // @author       Chr_
 // @include      https://live.bilibili.com/*
 // @license      AGPL-3.0
 // @icon         https://blog.chrxw.com/favicon.ico
-// @grant        GM_setValue
-// @grant        GM_getValue
 // ==/UserScript==
 
 (() => {
-    'use strict';
+    "use strict";
     //去除播放器的开关
-    let VEnable = false;
-    VEnable = GM_getValue('VE') == true;
+    let VEnable = window.localStorage.getItem("VEnable") === "true";
     if (VEnable) {
         setTimeout(() => {
-            document.getElementById('live-player').remove();
+            document.getElementById("live-player").remove();
         }, 3000);
     }
-    let btnArea = document.querySelector('.upper-right-ctnr');
-    let btn = document.createElement('button');
-    btn.id = 'removeLive';
-    btn.textContent = bool2txt(VEnable) + '移除播放器';
-    btn.addEventListener('click', removeLive);
-    btnArea.insertBefore(btn, btnArea.children[0]);
-    function removeLive() {
+    let btnArea = document.querySelector(".right-ctnr");
+    let btn = document.createElement("button");
+    btn.id = "removeLive";
+    btn.textContent = VEnable ? "恢复播放器" : "移除播放器";
+    btn.addEventListener("click", () => {
         VEnable = !VEnable;
-        GM_setValue('VE', VEnable);
-        btn.textContent = bool2txt(VEnable) + '移除播放器';
+        window.localStorage.setItem("VEnable", VEnable);
+        btn.textContent = VEnable ? "恢复播放器" : "移除播放器";
         if (VEnable) {
-            document.getElementById('live-player').remove();
+            document.getElementById("live-player").remove();
         } else {
             window.location.reload();
         }
-    }
-    // 显示布尔
-    function bool2txt(bool) {
-        return bool ? '【√】' : '【×】';
-    }
+    });
+    btnArea.insertBefore(btn, btnArea.children[0]);
 })();
 
