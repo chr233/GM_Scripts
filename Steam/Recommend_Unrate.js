eb459d84d63c543eb959a0f11a935f6d2d8ca764
Chr_ <chr@chrxw.com>
2022-03-06 01:03:21 +0800

    新增一条规则

--- Steam/Recommend_Unrate.js
+++ Steam/Recommend_Unrate.js
@@ -4,7 +4,7 @@
 // @namespace       https://blog.chrxw.com
 // @supportURL      https://blog.chrxw.com/scripts.html
 // @contributionURL https://afdian.net/@chr233
-// @version         1.0
+// @version         1.1
 // @description     批量撤回评测点赞/有趣
 // @description:zh-CN  批量撤回评测点赞/有趣
 // @author          Chr_
@@ -21,6 +21,7 @@
     "use strict";
 
     const defaultRules = [
+        "$$⠄|⢁|⠁|⣀|⣄|⣤|⣆|⣦|⣶|⣷|⣿|⣇|⣧",
         "pan",
         "share",
         "weiyun",
