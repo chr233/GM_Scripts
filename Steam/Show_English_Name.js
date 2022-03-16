e4382cdc7919b7db583004bde5b96d0abfa4fa66
Chr_ <chr@chrxw.com>
2022-03-17 00:11:13 +0800

    支持显示卡牌/中文支持信息

--- Steam/Show_English_Name.js
+++ Steam/Show_English_Name.js
@@ -4,7 +4,7 @@
 // @namespace       https://blog.chrxw.com
 // @supportURL      https://blog.chrxw.com/scripts.html
 // @contributionURL https://afdian.net/@chr233
-// @version         1.13
+// @version         1.15
 // @description     在商店页显示双语游戏名称，双击名称可以快捷搜索。
 // @description:zh-CN  在商店页显示双语游戏名称，双击名称可以快捷搜索。
 // @author          Chr_
@@ -20,20 +20,24 @@
     "use strict";
     const mode = window.localStorage.getItem("sen_mode") ?? "c(e)";
     const pure = window.localStorage.getItem("sen_pure") ?? "关";
+    const icon = window.localStorage.getItem("sen_icon") ?? "开";
+
     const appid = (window.location.pathname.match(/\/app\/(\d+)/) ?? [null, null])[1];
     if (appid === null) { return; }
     fetch(`https://store.steampowered.com/api/appdetails?appids=${appid}&l=english`)
         .then(async (response) => {
             if (response.ok) {
-                let json = await response.json();
-                let data = json[appid];
+                const json = await response.json();
+                const data = json[appid];
                 if (data.success !== true) { return; }
-                let name_en = data.data.name;
-                let t = setInterval(() => {
-                    let ele_title = document.getElementById("appHubAppName");
+
+                const { name: name_en, supported_languages, categories } = data.data;
+
+                const t = setInterval(() => {
+                    const ele_title = document.getElementById("appHubAppName");
                     if (ele_title != null) {
                         clearInterval(t);
-                        let ele_path = document.querySelector("div.blockbg>a:last-child");
+                        const ele_path = document.querySelector("div.blockbg>a:last-child");
                         let name_cur = ele_title.textContent
                         if (name_cur.toLowerCase() != name_en.toLowerCase()) {
                             if (pure === "开") {
@@ -46,27 +50,32 @@
                             } else {
                                 name_new = `${name_cur} (${name_en})`;
                             }
+
                             ele_title.textContent = name_new;
                             if (ele_path !== null) {
                                 ele_path.textContent = name_new;
                             }
                         }
+
+                        if (icon === "开") {
+                            if (supported_languages && supported_languages.search('Chinese')) {
+                                ele_title.textContent += "🀄";
+                            }
+                            if (categories && categories.some(c => c.id === 29)) {
+                                ele_title.textContent += "📇";
+                            }
+                        }
+
                         ele_title.title = "双击快捷搜索";
                         ele_title.addEventListener("dblclick", () => {
                             ShowConfirmDialog(`你想做什么呢？`, "", "复制游戏名", "搜索游戏名")
                                 .done(() => {
                                     const setClipboard = (data) => { GM_setClipboard(data, "text"); }
-                                    if (name_cur == name_en) {
-                                        setClipboard(name_cur);
-                                    } else {
+                                    if (name_cur == name_en) { setClipboard(name_cur); } else {
                                         ShowConfirmDialog(`要复制的游戏名称？`, "", name_cur, name_en)
-                                            .done(() => {
-                                                setClipboard(name_cur);
-                                            })
+                                            .done(() => { setClipboard(name_cur); })
                                             .fail((stats) => {
-                                                if (stats) {
-                                                    setClipboard(name_en);
-                                                }
+                                                if (stats) { setClipboard(name_en); }
                                             });
                                     }
                                 })
@@ -76,13 +85,9 @@
                                             window.open(`https://store.steampowered.com/search/?term=${name_cur}`);
                                         } else {
                                             ShowConfirmDialog(`要使用的搜索关键词？`, "", name_cur, name_en)
-                                                .done(() => {
-                                                    window.open(`https://store.steampowered.com/search/?term=${name_cur}`);
-                                                })
+                                                .done(() => { window.open(`https://store.steampowered.com/search/?term=${name_cur}`); })
                                                 .fail((stats) => {
-                                                    if (stats) {
-                                                        window.open(`https://store.steampowered.com/search/?term=${name_en}`);
-                                                    }
+                                                    if (stats) { window.open(`https://store.steampowered.com/search/?term=${name_en}`); }
                                                 });
                                         }
                                     }
@@ -100,11 +105,15 @@
     GM_registerMenuCommand(`切换显示格式：【${mode === "c(e)" ? "原名 (英文名)" : "英文名 (原名)"}】`, () => {
         window.localStorage.setItem("sen_mode", mode === "c(e)" ? "e(c)" : "c(e)");
         window.location.reload();
-    })
+    });
     GM_registerMenuCommand(`过滤特殊符号：【${pure}】`, () => {
         window.localStorage.setItem("sen_pure", pure === "开" ? "关" : "开");
         window.location.reload();
-    })
+    });
+    GM_registerMenuCommand(`显示卡牌图标：【${icon}】`, () => {
+        window.localStorage.setItem("sen_icon", icon === "开" ? "关" : "开");
+        window.location.reload();
+    });
     function pureName(str) {
         return str.replace(/[《》™©®]/g, "");
     }
