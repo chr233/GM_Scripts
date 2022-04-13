156ac6d20f2c76f9ec3d21cc38381c3300021592
Chr_ <chr@chrxw.com>
2022-04-13 11:32:14 +0800

    更新数据

--- Keylol/Hikari_Field_Helper.js
+++ Keylol/Hikari_Field_Helper.js
@@ -4,7 +4,7 @@
 // @namespace       https://blog.chrxw.com
 // @supportURL      https://blog.chrxw.com/scripts.html
 // @contributionURL https://afdian.net/@chr233
-// @version         2.9
+// @version         2.10
 // @description     Hikari_Field入库游戏检测
 // @description:zh-CN  Hikari_Field入库游戏检测
 // @author          Chr_
