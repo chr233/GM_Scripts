f4eeb59f712b9b7753b054eb9104aecaf0054b52
Chr_ <chr@chrxw.com>
2022-05-16 20:31:45 +0800

    修正翻译错误

--- SteamDB/SteamDB_CN.js
+++ SteamDB/SteamDB_CN.js
@@ -4,7 +4,7 @@
 // @namespace           https://blog.chrxw.com
 // @supportURL          https://blog.chrxw.com/scripts.html
 // @contributionURL     https://afdian.net/@chr233
-// @version             1.30
+// @version             1.31
 // @description         SteamDB汉化插件
 // @description:zh-cn   SteamDB汉化插件
 // @author              Chr_
