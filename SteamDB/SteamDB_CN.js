c7f8750619cb0e8438429777592252a7fac04260
Chr_ <chr@chrxw.com>
2023-02-16 23:01:48 +0800

    更新汉化

--- SteamDB/SteamDB_CN.js
+++ SteamDB/SteamDB_CN.js
@@ -4,7 +4,7 @@
 // @namespace           https://blog.chrxw.com
 // @supportURL          https://blog.chrxw.com/scripts.html
 // @contributionURL     https://afdian.net/@chr233
-// @version             1.32
+// @version             1.33
 // @description         SteamDB汉化插件
 // @description:zh-cn   SteamDB汉化插件
 // @author              Chr_
