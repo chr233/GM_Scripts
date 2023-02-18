3aa05f48e27d7721450c6746018b78b596eb46f9
Chr_ <chr@chrxw.com>
2023-02-19 01:19:20 +0800

    update

--- Keylol/Am_I_Replied.js
+++ Keylol/Am_I_Replied.js
@@ -4,7 +4,7 @@
 // @namespace       https://blog.chrxw.com
 // @supportURL      https://blog.chrxw.com/scripts.html
 // @contributionURL https://afdian.net/@chr233
-// @version         1.4
+// @version         1.5
 // @description     判断是否已经回过贴
 // @description:zh-CN  判断是否已经回过贴
 // @author          Chr_
@@ -15,10 +15,20 @@
 // @include         https://nga.178.com/read.php?*
 // @license         AGPL-3.0
 // @icon            https://blog.chrxw.com/favicon.ico
+// @grant           GM_registerMenuCommand
 // ==/UserScript==
 
 (async () => {
   "use strict";
+
+  const inlineMode = window.localStorage.getItem("air_inline") ?? "关";
+  GM_registerMenuCommand(`行内显示已回复: 【${inlineMode}】`, () => {
+    window.localStorage.setItem(
+      "air_inline",
+      inlineMode === "开" ? "关" : "开"
+    );
+    window.location.reload();
+  });
 
   if (
     (location.pathname === "/forum.php" && !location.search.includes("tid")) ||
@@ -43,9 +53,12 @@
     .then((res) => res.text())
     .then((html) => {
       const replied = !(html.includes("未定义操作") || html.includes("ERROR:"));
-      const btnArea = document.querySelector(
-        "#postlist td.plc div.authi>span.none, #m_nav>.nav"
-      );
+      const query =
+        inlineMode === "开" ? "#postlist td.plc div.authi>span.none" : "#pgt";
+      const btnArea =
+        document.querySelector(query) ??
+        document.querySelector("#postlist td.plc div.authi>span.pipe") ??
+        document.querySelector("#m_nav>.nav");
       const text = replied ? "✅已经回过贴了" : "❌还没回过贴子";
 
       const tips = document.createElement("a");
@@ -58,7 +71,9 @@
         });
       }
 
-      if (isDiscuz) {
+      console.log(btnArea.tagName);
+
+      if (isDiscuz && btnArea.tagName === "SPAN") {
         const span = document.createElement("span");
         span.textContent = "|";
         span.className = "pipe";
