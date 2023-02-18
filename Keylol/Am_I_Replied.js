2042df2cccd13db71cd5024322ca6da1e02678f2
Chr_ <chr@chrxw.com>
2023-02-19 01:02:14 +0800

    update

--- Keylol/Am_I_Replied.js
+++ Keylol/Am_I_Replied.js
@@ -4,7 +4,7 @@
 // @namespace       https://blog.chrxw.com
 // @supportURL      https://blog.chrxw.com/scripts.html
 // @contributionURL https://afdian.net/@chr233
-// @version         1.3
+// @version         1.4
 // @description     判断是否已经回过贴
 // @description:zh-CN  判断是否已经回过贴
 // @author          Chr_
@@ -18,38 +18,55 @@
 // ==/UserScript==
 
 (async () => {
-    "use strict";
+  "use strict";
 
-    if ((location.pathname === "/forum.php" && !location.search.includes("tid")) || location.search.includes("authorid")) {
-        return;
-    }
+  if (
+    (location.pathname === "/forum.php" && !location.search.includes("tid")) ||
+    location.search.includes("authorid")
+  ) {
+    return;
+  }
 
-    const userId = typeof discuz_uid != 'undefined' ? discuz_uid : __CURRENT_UID;
+  const isDiscuz = typeof discuz_uid != "undefined";
 
-    let testUrl = location.href;
+  const userId = isDiscuz ? discuz_uid : __CURRENT_UID;
 
-    if (location.search) {
-        testUrl += `&authorid=${userId}`;
-    } else {
-        testUrl += `?authorid=${userId}`;
-    }
+  let testUrl = location.href;
 
-    fetch(testUrl)
-        .then((res) => res.text())
-        .then((html) => {
-            const replied = !(html.includes("未定义操作") || html.includes("ERROR:"));
-            const btnArea = document.querySelector("#pgt, #m_nav>.nav");
-            const text = replied ? "✅已经回过贴了" : "❌还没回过贴子";
-            const tips = document.createElement("a");
-            tips.textContent = text;
-            if (replied) {
-                tips.href = testUrl;
-            }
-            else {
-                tips.addEventListener("click", () => {
-                    showError("❌还没回过贴子");
-                });
-            }
-            btnArea.appendChild(tips);
+  if (location.search) {
+    testUrl += `&authorid=${userId}`;
+  } else {
+    testUrl += `?authorid=${userId}`;
+  }
+
+  fetch(testUrl)
+    .then((res) => res.text())
+    .then((html) => {
+      const replied = !(html.includes("未定义操作") || html.includes("ERROR:"));
+      const btnArea = document.querySelector(
+        "#postlist td.plc div.authi>span.none, #m_nav>.nav"
+      );
+      const text = replied ? "✅已经回过贴了" : "❌还没回过贴子";
+
+      const tips = document.createElement("a");
+      tips.textContent = text;
+      if (replied) {
+        tips.href = testUrl;
+      } else {
+        tips.addEventListener("click", () => {
+          showError("❌还没回过贴子");
         });
+      }
+
+      if (isDiscuz) {
+        const span = document.createElement("span");
+        span.textContent = "|";
+        span.className = "pipe";
+        const bar = btnArea.parentNode;
+        bar.insertBefore(span, btnArea);
+        bar.insertBefore(tips, btnArea);
+      } else {
+        btnArea.appendChild(tips);
+      }
+    });
 })();
