1eb5f3f6e7658f470669eb26a9dc06cfd58da9ab
Chr_ <chr@chrxw.com>
2024-01-24 15:03:49 +0800

    fix

--- Sonkwo/Auto_Sign.js
+++ Sonkwo/Auto_Sign.js
@@ -2,7 +2,7 @@
 // @name         Sonkwo_Auto
 // @name:zh-CN   杉果自动签到
 // @namespace    https://blog.chrxw.com/
-// @version      0.2
+// @version      0.3
 // @description  杉果自动签到
 // @author       Chr_
 // @license      AGPL-3.0
@@ -13,8 +13,8 @@
 (function () {
     "use strict";
     setTimeout(() => {
-        const sign = document.querySelector('div.store_user_card_action_check');
-        if (sign && sign.textContent == '签到') {
+        const sign = document.querySelector('div.SK-button-com.sign-btn>button');
+        if (sign && sign.textContent == '签到赚积分') {
             sign.click();
             setTimeout(() => {
                 window.location.reload();
