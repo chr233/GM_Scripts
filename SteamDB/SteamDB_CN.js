50e9e20150d89161b7fb97f28fb42d96f9d4ebbe
Chr_ <chr@chrxw.com>
2024-06-27 23:31:43 +0800

    fix

--- SteamDB/SteamDB_CN.js
+++ SteamDB/SteamDB_CN.js
@@ -4,7 +4,7 @@
 // @namespace           https://blog.chrxw.com
 // @supportURL          https://blog.chrxw.com/scripts.html
 // @contributionURL     https://afdian.net/@chr233
-// @version             1.35
+// @version             1.36
 // @description         SteamDB汉化插件
 // @description:zh-cn   SteamDB汉化插件
 // @author              Chr_
@@ -22,6 +22,11 @@
   "use strict";
   const DEBUG = window.localStorage["dbg_mode"] == "开";
   const OUTPUT = window.localStorage["out_word"] == "开";
+
+  if (location.search.includes("__cf_chl_tk")) {
+    console.log("检测到CF盾, 不执行脚本");
+    return;
+  }
 
   GM_registerMenuCommand(`调试汉化文本: 【${DEBUG ? "开" : "关"}】`, () => {
     window.localStorage["dbg_mode"] = DEBUG ? "关" : "开";
